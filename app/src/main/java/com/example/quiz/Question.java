09c7cb826fb40e27066c7d123336b018edf9bab1
Karol <michkarol.2000@wp.pl>
2021-10-28 14:47:50 +0200

    Initial commit

---
+++ app/src/main/java/com/example/quiz/Question.java
@@ -0,0 +1,23 @@
+package com.example.quiz;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Question {
+    private int questionId;
+    private boolean trueAnswer;
+
+    public Question(int questionId, boolean trueAnswer){
+        this.questionId=questionId;
+        this.trueAnswer=trueAnswer;
+    }
+
+    public boolean isTrueAnswer(){
+        return this.trueAnswer;
+    }
+    public int getQuestionId(){
+        return this.questionId;
+    }
+}
